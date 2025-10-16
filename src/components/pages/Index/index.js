664422b1c61a5d0ac6e1c0afd9bd1a1c0be36b4e
Elcheno <r.c.a2312@gmail.com>
2025-10-16 19:57:16 +0200

    change foto

--- src/components/pages/Index/index.js
+++ src/components/pages/Index/index.js
@@ -1,9 +1,8 @@
 import Intro from "./Intro/Intro.astro";
 import NavigationBar from "./NavigationBar/NavigationBar.astro";
-import Body from "./Body/Body.astro";
 import InfiniteText from "./InfiniteText/InfiniteText.astro";
 import Galery from "./Galery/Galery.astro";
 import About from "./About/About.astro";
 import Contact from "./Contact/Contact.astro";
 
-export { Intro, NavigationBar, Body, InfiniteText, Galery, About, Contact };
+export { Intro, NavigationBar, InfiniteText, Galery, About, Contact };
